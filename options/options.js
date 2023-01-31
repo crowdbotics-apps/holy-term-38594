5f267178787d38e0782c277b82abd29cfeaa8d9f
Crowdbotics <team@crowdbotics.com>
2023-01-31 06:53:21 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "holy-term-38594",
+  url: "https://holy-term-38594.botics.co",
+  api: "https://holy-term-38594.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
